658803f10ab6209c3e6a8f8f4fc6dc6b310e7ad1
balefra <franedubale23@gmail.com>
2024-10-19 17:11:40 -0500

    actualizacion

--- iberoV2-app/resources/views/Realidad.blade.php
+++ iberoV2-app/resources/views/Realidad.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/estiloss.css') }}">
 </head>
 
 <body>
@@ -18,8 +18,8 @@
         <h1>Modelado Investigación</h1>
     </div>
     <div class="container">
-        <img src="{{ asset('assets/iberov2.png') }}"  class="iberov">
-        <img src="{{ asset('assets/uNIVERSIDA TRES CULTURAS.png') }}" class="utc">
+        <img src="{{ asset('assets/images/iberov2.png') }}"  class="iberov">
+        <img src="{{ asset('assets/images/uNIVERSIDA TRES CULTURAS.png') }}" class="utc">
     </div>
 
     
--- iberoV2-app/routes/web.php
+++ iberoV2-app/routes/web.php
@@ -1,31 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\PostController;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Auth::routes();
 
-Route::get('/Metodologico', function () {
-    return view('Metodologico');
-})->name('Metodologico');
-
-Route::get('/Realidad', function () {
-    return view('Realidad');
-})->name('Realidad');
-
-Route::get('/formulacion', function () {
-    return view('formulacion.formulacion');
-})->name('formulacion');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/crear', function () {
     return view('posts.crear');
 })->name('Crear');
-
-Route::get('/welcome', function () {
-    return view('welcome');
-})->name('welcome');
-
 Route::post('/', [PostController::class, 'store']);
+
+Route::get('/Realidad', function () {
+    return view('Realidad');
+})->name('Realidad');
